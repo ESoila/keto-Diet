62b49fc2580b2c8a69b691ff6049d539df007572
soila <elizabeth.soila@student.moringaschool.com>
2024-12-20 10:00:07 +0300

    addingcomments

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+// defining endpoint
 const API_URL = 'https://www.themealdb.com/api/json/v1/1/';
 
+// give instructions on what to do after loading page.
 document.addEventListener("DOMContentLoaded", () => {
     fetchCategories();
     loadAllReviews();
 });
-
+// allows page to fetch categories and display them
 async function fetchCategories() {
     try {
       const endpoint = `${API_URL}categories.php`;  
@@ -12,34 +14,27 @@ async function fetchCategories() {
       const data = await response.json();
       displayCategories(data.categories);
     } catch (error) {
-        
+        console.log("Error Loading");
     }
 }
+// display categories.
 function displayCategories(categories){
     const categoriesList = document.getElementById("categories"); 
     const categoriesCard = document.getElementById("categories-details");
-
+// looping through each category
     categories.forEach(category => {
         const card = categoriesCard.cloneNode (true);
         card.style.display = 'block';
         card.querySelector('#strcategory-title').textContent = category.strCategory;
         card.querySelector('#strcategorythumb').src = category.strCategoryThumb;
         card.querySelector('#strcategorydescription').textContent = category.strCategoryDescription.substring(0, 120) + '..';
-
+// on click event fetch recipe by category
         card.addEventListener('click', () => fetchingRecipeByCategory(category.strCategory));
         categoriesList.appendChild(card);
 
     }); 
 }
-function showSection(sectionId) {
-    //.Render the menu sections.
-    const sections = document.querySelectorAll("section, #movie-details");
-    sections.forEach(section => section.style.display = "none");
-    const section = document.getElementById(sectionId);
-    if (section) {
-        section.style.display = "block";
-    }
-}
+// Allows page to fetch recipe by category and dispay
 async function fetchingRecipeByCategory(category) {
     try {
         const endpoint = `${API_URL}filter.php?c=${encodeURIComponent(category)}`; 
@@ -48,29 +43,31 @@ async function fetchingRecipeByCategory(category) {
         const recipesToDisplay = data.meals.slice(0, 4);
         displayRecipesByCategory(recipesToDisplay);
     } catch (error) {
-        
+        console.log("Error Loading");
     }
 }
+// display Recipes by category
 function displayRecipesByCategory(recipes) {
     const recipeDetails = document.getElementById("recipe-details");
     recipeDetails.innerHTML = '';
-
+// loop through each recipe on the category
     recipes.forEach(recipe => {
         const recipeCard = document.createElement('div');
         recipeCard.classList.add('recipe-card');
-
+// shows attributes to display on recipe card
         recipeCard.innerHTML = `
           <img src = "${recipe.strMealThumb}">
           <h3>${recipe.strMeal}</h3>
           <h3>${recipe.idMeal}</h3>
         `;
-        
+// shows receipe detail when you click recipe card
         recipeCard.addEventListener('click', () => fetchRecipeDetails(recipe.idMeal));
         recipeDetails.classList.remove("hidden");
       recipeDetails.appendChild(recipeCard); 
 
     });
 }
+// fetching recipe details by id
 async function fetchRecipeDetails(id) {
     try {
         const endpoint = `${API_URL}lookup.php?i=${id}`;
@@ -78,9 +75,10 @@ async function fetchRecipeDetails(id) {
         const data = await response.json();
         displayRecipeDetails(data.meals[0]);
     } catch (error) {
-        
+        console.log("Error Loading");
     }
 }
+// display recipe details
 function displayRecipeDetails(meal) {
     const recipeDetails = document.getElementById("recipe-details");
     recipeDetails.innerHTML = `
@@ -106,6 +104,7 @@ function displayRecipeDetails(meal) {
     `;
     loadReviewsForRecipe(meal.idMeal);
 }
+// add ingredients list by looping through the pay load
 function getIngredList(meal) {
     let ingredients = '';
     for (let i = 1; i <= 20; i++) {
@@ -118,24 +117,34 @@ function getIngredList(meal) {
     }
     return ingredients;
 }
+// reviews box text and functions.
 function addReview(id) {
     const reviewText = document.getElementById('review-text').value;
     if (!reviewText)
         return;
 
     const reviews = getReviewFromLocalStorage();
-    if (!reviews[id]){
-        reviews[id] = [];
+    const editIndex = document.getElementById('review-text').dataset.editIndex;
+    if (editIndex){
+        reviews[id][editIndex] = reviewText;
+        delete document.getElementById('review-text').dataset.editIndex;
+    } else { 
+        if (!reviews[id]){
+            reviews[id] = [];
+        }
+        reviews[id].push(reviewText);
     }
-    reviews[id].push(reviewText);
+   
     localStorage.setItem('reviews', JSON.stringify(reviews));
     loadReviewsForRecipe(id);
     document.getElementById('review-text').value = '';
 }
+// get stored review retrival
 function getReviewFromLocalStorage(){
     const reviews = localStorage.getItem('reviews');
     return reviews ? JSON.parse(reviews) : {};
 }
+// front face review viwer and delete.
 function loadReviewsForRecipe(recipeId){
     const reviews = getReviewFromLocalStorage();
     const reviewList = document.getElementById('review-list');
@@ -145,12 +154,14 @@ function loadReviewsForRecipe(recipeId){
             const reviewItem = document.createElement('li');
             reviewItem.innerHTML = `
                 <p>${review}</p>
+                <button onclick="editReviews('${recipeId}',${index})">Edit</button>
                 <button onclick="deleteReviews('${recipeId}',${index})">Delete</button>
             `;
             reviewList.appendChild(reviewItem);
         });
     }
 }
+// allows all reviews to load.
 function loadAllReviews() {
    const recipe = document.querySelectorAll('.recipe-card');
    recipe.forEach(rcp =>{
@@ -158,6 +169,14 @@ function loadAllReviews() {
     loadReviewsForRecipe(id);
    });
 }
+// Edit Reviews
+function editReviews(id,index) {
+    const reviews = getReviewFromLocalStorage();
+    const reviewText = reviews[id] [index];
+    document.getElementById('review-text').value = reviewText;
+    document.getElementById('review-text').dataset.editIndex = index;
+}
+// delete reviews.
 function deleteReviews(id, index) {
     const reviews = getReviewFromLocalStorage();
     reviews[id].splice(index, 1);
